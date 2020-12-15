01ec880850ec8b095d55fd092ea38f47bf6737e3
CactusKipic <clement.azan@gmail.com>
2020-12-15 16:18:47 +0100

    Modification du script de mouvement: Mouvement haut bas + contrôles par "gradients" (valeur entre -1 et 1)

--- Assets/Scripts/PlaneMovements.cs
+++ Assets/Scripts/PlaneMovements.cs
@@ -5,41 +5,53 @@ using UnityEngine;
 public class PlaneMovements : MonoBehaviour {
 
     // Rotation
-    public float m_rotationAcceleration = 1.5f; // Accélération pour atteindre la vitesse de rotation de "croisière"
-    private float m_actualRotation = 0.0f;
-    public float m_rotationSpeed = 5.0f; // Vitesse de rotation de "croisière"
-    public float m_up_down = 0.0f; // Gradient Haut/bas
-    public float m_left_right = 0.0f; // Gradient Gauche/droite
-    private float m_angle = 0.0f;
+    public float m_rotationAccelerationLR = 1.5f; // Accélération pour atteindre la vitesse de roulis de "croisière"
+    public float m_rotationAccelerationUD = 1.0f; // Accélération pour atteindre la vitesse de tangage de "croisière"
+    private float m_actualRotation_LR = 0.0f; // Left/Right
+    private float m_actualRotation_UD = 0.0f; // Up/Down
+    public float m_rotationSpeedLR = 5.0f; // Vitesse de roulis de "croisière"
+    public float m_rotationSpeedUD = 5.0f; // Vitesse de tangage de "croisière"
+
+    private float m_angleLR = 0.0f; // Angle actuel Gauche/Droite
+    private float m_angleUD = 0.0f; // Angle actuel Haut/Bas
     // Speed
     public float m_accelerationSpeed = 0.3f; // Accélération pour atteindre la vitesse de "croisière"
     private float m_actualSpeed = 0.0f;
     public float m_speedMax = 1.0f; // Vitesse de "croisière"
-    public float m_speed = 0.0f;
+
     private Vector3 m_vector3;
+
+    //Commands
+    public float m_up_down = 0.0f; // Gradient Haut/bas
+    public float m_left_right = 0.0f; // Gradient Gauche/droite
+    public float m_speed = 0.0f; // Gradient de vitesse
+
 
     // Start is called before the first frame update
     void Start() {
         m_vector3 = this.transform.localPosition;
-        m_angle = this.transform.localRotation.y;
+        m_angleLR = this.transform.localRotation.y;
+        m_angleUD = this.transform.localRotation.z;
     }
 
     // Update is called once per frame
     void Update() {
 
         // Rotation
-        m_actualRotation += (m_rotationSpeed > m_actualRotation ? m_rotationAcceleration : -m_rotationAcceleration) * Time.deltaTime;
+        m_actualRotation_LR += ((m_rotationSpeedLR * m_left_right) > m_actualRotation_LR ? m_rotationAccelerationLR : -m_rotationAccelerationLR) * Time.deltaTime;
+        m_actualRotation_UD += ((m_rotationSpeedUD * m_up_down) > m_actualRotation_UD ? m_rotationAccelerationUD : -m_rotationAccelerationUD) * Time.deltaTime;
 
-        m_angle += m_actualRotation * Time.deltaTime;
+        m_angleLR += m_actualRotation_LR * Time.deltaTime;
 
-        this.transform.localRotation = Quaternion.Euler((m_actualRotation * -1), m_angle, 0.0f);
+        this.transform.localRotation = Quaternion.Euler((m_actualRotation_LR * -1), m_angleLR, m_actualRotation_UD);
 
         // Speed
 
-        m_actualSpeed += (m_speed > m_actualSpeed ? m_accelerationSpeed : -m_accelerationSpeed) * Time.deltaTime;
+        m_actualSpeed += ((m_speedMax * m_speed) > m_actualSpeed ? m_accelerationSpeed : -m_accelerationSpeed) * Time.deltaTime;
 
-        m_vector3.x = m_vector3.x + m_actualSpeed * Mathf.Cos(m_angle * Mathf.Deg2Rad) * Time.deltaTime;
-        m_vector3.z = m_vector3.z + m_actualSpeed * -Mathf.Sin(m_angle * Mathf.Deg2Rad) * Time.deltaTime;
+        m_vector3.x += m_actualSpeed * Mathf.Cos(m_angleLR * Mathf.Deg2Rad) * Mathf.Cos(m_actualRotation_UD * Mathf.Deg2Rad) * Time.deltaTime; // Axe X
+        m_vector3.z += m_actualSpeed * -Mathf.Sin(m_angleLR * Mathf.Deg2Rad) * Mathf.Cos(m_actualRotation_UD * Mathf.Deg2Rad) * Time.deltaTime; // Axe Z
+        m_vector3.y += m_actualSpeed * Mathf.Sin(m_actualRotation_UD * Mathf.Deg2Rad) * Time.deltaTime; // Up and Down
 
         this.transform.localPosition = m_vector3;
 
